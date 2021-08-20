9972238f5d90081a078cadaeba75e5faf76426e8
trivendra kulhare <trivendrakulhare@gmail.com>
2021-08-20 21:03:07 +0200

    Modified some code and added CMake

---
+++ build/CMakeFiles/stateMachine.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stateMachine.dir/src/CountDown.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/CountDown.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/FairingJettison.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/FairingJettison.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/InitialEngineIgnition.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/InitialEngineIgnition.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/LaunchPadRelease.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/LaunchPadRelease.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/NextEngine.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/NextEngine.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/PayloadRealease.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/PayloadRealease.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/Termination.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/Termination.cpp.o.d"
+  "CMakeFiles/stateMachine.dir/src/main.cpp.o"
+  "CMakeFiles/stateMachine.dir/src/main.cpp.o.d"
+  "stateMachine"
+  "stateMachine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stateMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/stateMachine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stateMachine.
+# This may be replaced when dependencies are built.
